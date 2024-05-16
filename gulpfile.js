6f0ef8528f239dc8de4ec123a35814fbdb70352a
Jonathan Ortiz <jonathanor9808@gmail.com>
2024-05-15 23:10:48 -0500

    FINALIZADO

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-import {src, dest, watch} from 'gulp'
+import {src, dest, watch , series} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
+export function js (done){
+    src('src/js/app.js')
+    .pipe(dest('build/js'))
+    done()
+}
 export function css (done){
 
     src('src/scss/app.scss', {sourcemaps: true})
@@ -16,4 +21,7 @@ export function css (done){
 
 export function dev(){
 watch('src/scss/**/*.scss', css)
+watch('src/js/**/*.js', js)
 }
+
+export default series(js , css, dev) // aranca una tarea la finaliza y sigue con la otra
