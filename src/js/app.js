ace6aa5355dbf2fc5dcdd2f57e9724a88d06251d
Jonathan Ortiz <jonathanor9808@gmail.com>
2024-05-16 22:02:38 -0500

    FIN

--- src/js/app.js
+++ src/js/app.js
@@ -22,9 +22,14 @@ function crearGaleria() {
   const cantidad_imaganes = 16;
 
   for (let i = 1; i <= cantidad_imaganes; i++) {
-    const imagen = document.createElement("IMG");
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = `Imagen Galeria`;
+    
+    const imagen = document.createElement("PICTURE");
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
+    
 
     // event handler responde a un clinck en una imagen
     imagen.onclick = function () {
@@ -35,9 +40,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-  const imagen = document.createElement("IMG");
-  imagen.src = `src/img/gallery/full/${i}.jpg`;
-  imagen.alt = `Imagen Galeria`;
+  const imagen = document.createElement("PICTURE");
+  imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
   // generar modal
   const modal = document.createElement("DIV");
   modal.classList.add("modal");
